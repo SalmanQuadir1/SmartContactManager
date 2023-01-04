c93437f3bd7e34b516af077502b2c81f08018d28
salman <najarsalman4@gmail.com>
2023-01-04 20:11:58 +0530

    new

--- SmartContactManager/src/main/java/com/SmartContactManager/controllers/HomeController.java
+++ SmartContactManager/src/main/java/com/SmartContactManager/controllers/HomeController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.SmartContactManager.dao.UserRepository;
 import com.SmartContactManager.entities.Contact;
@@ -17,21 +19,26 @@ public class HomeController {
 	private UserRepository userrepo;
 
 	@RequestMapping("/test")
-	@ResponseBody
-	public String test() {
+	public String test(Model model) {
 		User user = new User();
 		user.setName("Salman");
-		Contact cont=new Contact();
+		Contact cont = new Contact();
 		cont.setDescription("desc");
 		cont.setEmail("email@gmail.com");
 		cont.setPhone("7006584939");
 		user.setEmail("email@gmail.com");
 		cont.setUser(user);
-		List<Contact> contt=List.of(cont);
+		List<Contact> contt = List.of(cont);
 		user.setContact(contt);
 		userrepo.save(user);
+		model.addAttribute("user", user);
 
-		return "Working...";
+		return "views/home";
+	}
+
+	@RequestMapping("/")
+	public String login() {
+		return "login";
 	}
 
 }
