e32215ca3dcbd216a570a7e7c0926f413b7fe327
salman <najarsalman4@gmail.com>
2023-01-19 20:14:09 +0530

    image added

--- SmartContactManager/src/main/java/com/SmartContactManager/controllers/UserController.java
+++ SmartContactManager/src/main/java/com/SmartContactManager/controllers/UserController.java
@@ -1,14 +1,22 @@
 package com.SmartContactManager.controllers;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.SmartContactManager.dao.UserRepository;
 import com.SmartContactManager.entities.Contact;
@@ -42,12 +50,26 @@ public class UserController {
 	}
 	
 	@PostMapping("/process-contact")
-	public String AddContactForm(@ModelAttribute Contact contact,Principal principal) {
+	public String AddContactForm(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file,Principal principal) {
+		try {
 		User user = this.userrepo.getUserByUserName(principal.getName());
+		
+		if(file.isEmpty()) {
+			System.out.println("File Empty!!");
+		}else {
+			contact.setImage(file.getOriginalFilename());
+			
+			File saveFile=new ClassPathResource("static/images").getFile();
+			Path path=Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+		}
 		contact.setUser(user);
 		user.getContact().add(contact);
 		this.userrepo.save(user);
 		System.out.println(contact);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		return "views/add_contact_form";
 	}
 
